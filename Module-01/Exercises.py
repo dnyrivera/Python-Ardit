553ca287de7b3dec58a27f4c361cc519d582cee8
Donny Rivera <donny.rivera@gmail.com>
2025-04-28 01:57:54 -0400

    :white_check_mark:Module01:Day03

--- Module-01/Exercises.py
+++ Module-01/Exercises.py
@@ -22,5 +22,23 @@
 (1) prints out the methods of the list type, and
 (2) prints out the help of the list.count method
 """
-print(dir(list))
-print(help(list.count))
+# print(dir(list))
+# print(help(list.count))
+
+"""
+(1) iterates over the items of the member list
+(2) prints out each item of the list with the first letter capitalized
+
+In other words, your program should print out : John / Sarah / Dora
+"""
+# members = ["john", "sarah", "dora"]
+# for member in members:
+#     print(member.capitalize())
+
+"""
+(1) iterates over the scores list, and 
+(2) prints out each item.
+"""
+scores = [11,34,98,43,45,54,54]
+for score in scores:
+    print(score)
--- Module-01/Project-ToDo/main.py
+++ Module-01/Project-ToDo/main.py
@@ -1,6 +1,19 @@
 todos = []
 
 while True:
-    todo = input("Enter a todo:").strip().title()
-    todos.append(todo)
-    print(todos)
+    user_option = input(
+        "Choose your option: Add / List / Exit: ").strip().upper()
+
+    match user_option:
+        case 'ADD':
+            todo = input("Enter your task: ").strip().title()
+            todos.append(todo)
+            print("Task Added.")
+        case 'LIST':
+            for index, item in enumerate(todos, start=1):
+                print(index, item, sep='->')
+        case 'EXIT':
+            break
+        case _:
+            print("Invalid Option.")
+print("System Closed...")
